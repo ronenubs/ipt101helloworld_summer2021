dbd223f933fbef77198f361b65ccf1134f6f0ec5
Ron Enubs <ronenubs@gmail.com>
2021-06-15 09:32:22 +0800

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Person
+    {
+        public Person(string firstName, string lastName, int age, string address)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.address = address;
+        }
+
+        private string firstName { get; set; }
+        private string lastName { get; set; }
+        private int age { get; set; }
+        private string address { get; set; }
+
+        
+    }
+}
